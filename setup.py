f5617b0d706096dbfe785f2962c14e8b2a19cd11
Gerhard Weis <g.weis@griffith.edu.au>
2014-03-06 00:27:47 +1000

    prepare release 2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0dev'
+version = '2.0'
 maintainer = 'Gerhard Weis'
 
 setup(name='collective.js.uix.multiselect',
@@ -43,4 +43,3 @@ setup(name='collective.js.uix.multiselect',
 target = plone
 ''',
       )
-
